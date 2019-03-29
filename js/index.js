43e13b08cd12b8dc31363d70395bdebdac23c5f8
kishan-mund <kishan.mund@mountblue.io>
2019-03-29 13:49:12 +0530

    Extra loop removed from updateElement()

--- js/index.js
+++ js/index.js
@@ -7,92 +7,109 @@ let cardId = "5c99ac9a41258086d689d553";
 let checklistId = "5c99ace980151083b6e76c2a";
 let arr = [];
 
-
-let fetchArr = async () => {
-    let arr1 = [];
-    let data1 = await fetch('https://api.trello.com/1/checklists/' +
-        checklistId + '/checkItems?key=' + key + '&token=' + token);
-    let data = await data1.json();
-    for (let i of data) {
-        arr1.push(i);
-    }
-    return arr1;
-}
-const operations = (arr) => {
+const fetchArr = () => {
     return new Promise((resolve, reject) => {
-        // console.log(arr);
-        checkboxElement(arr);
-        updateElement(arr);
-        deleteElement(arr);
-    })
-}
-let checkboxElement = (arr) => {
-    console.log(length);
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr);
-        let cb = document.createElement("input");
-        let cb1 = document.createElement("label");
-        let span = document.createElement("span");
-        let newdiv = document.createElement("div");
-        cb.setAttribute("type", "checkbox");
-        span.className = "close-span";
-        cb.className = "radio";
-        newdiv.className = "newdiv";
-        span.className = "close";
-        span.textContent = "\u00D7";
-        if (arr[i].state === "complete") {
-            cb.checked = true;
+        let arr1 = [];
+        fetch('https://api.trello.com/1/checklists/' + checklistId + '/checkItems?key=' + key + '&token=' + token)
+            .then(data => data.json())
+            .then(data => {
+                for (let i of data) {
+                    arr1.push(i);
+                }
+                resolve(arr1);
+            });
+    });
+};
+
+const operations = arr => {
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < arr.length; i++) {
+            console.log(arr);
+            let cb = document.createElement("input");
+            let cb1 = document.createElement("label");
+            let span = document.createElement("span");
+            let newdiv = document.createElement("div");
+            cb.setAttribute("type", "checkbox");
+            span.className = "close-span";
+            cb.className = "radio";
+            newdiv.className = "newdiv";
+            span.className = "close";
+            span.textContent = "\u00D7";
+            if (arr[i].state === "complete") {
+                cb.checked = true;
+            }
+            cb1.textContent = arr[i].name;
+            document.getElementById("list").appendChild(newdiv);
+            newdiv.appendChild(cb);
+            newdiv.appendChild(cb1);
+            newdiv.appendChild(span);
+
+            cb.addEventListener("click", async function (event) {
+                arr = await updateElement(event, arr, i);
+            });
+            span.addEventListener("click", async function (event) {
+                arr = await deleteElement(event, arr, i);
+            });
         }
-        cb1.textContent = arr[i].name;
-        document.getElementById("list").appendChild(newdiv);
-        newdiv.appendChild(cb);
-        newdiv.appendChild(cb1);
-        newdiv.appendChild(span);
-    }
+    });
+};
 
-}
-let deleteElement = (arr) => {
-    let close = document.querySelectorAll(".close");
-    console.log(close);
-    for (let i = 0; i < close.length; i++) {
-        close[i].addEventListener("click", function (event) {
-            event.target.parentElement.remove();
-            var data = null;
-            var xhr = new XMLHttpRequest();
-            xhr.addEventListener("readystatechange", function () {
-                if (this.readyState === this.DONE) {
-                    console.log(this.responseText);
-                }
-            });
-            xhr.open("DELETE", "https://api.trello.com/1/checklists/" + checklistId +
-                "/checkItems/" + arr[i].id + "?key=" + key + "&token=" + token);
-            xhr.send(data);
-        })
+const deleteElement = (event, arr, index) => {
+    return new Promise((resolve, reject) => {
+        var data = null;
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                console.log(this.responseText);
+            }
+        });
+        xhr.open(
+            "DELETE",
+            "https://api.trello.com/1/checklists/" +
+            checklistId +
+            "/checkItems/" +
+            arr[index].id +
+            "?key=" +
+            key +
+            "&token=" +
+            token
+        );
+        xhr.send(data);
+        event.target.parentElement.remove();
 
-    }
-}
+        resolve(fetchArr());
+    });
+};
 
-let updateElement = (arr) => {
-    let radio = document.querySelectorAll(".radio");
-    for (let i = 0; i < radio.length; i++) {
-        radio[i].addEventListener("click", function (event) {
-            let state = '';
-            console.log(radio);
+let updateElement = (event, arr, index) => {
+    return new Promise((resolve, reject) => {
+        let state = "";
 
-            radio[i].checked ? state = 'complete' : state = 'incomplete'
-            var data = null;
-            var xhr = new XMLHttpRequest();
-            xhr.addEventListener("readystatechange", function () {
-                if (this.readyState === this.DONE) {
-                    console.log(this.responseText);
-                }
-            });
-            xhr.open("PUT", "https://api.trello.com/1/cards/" + cardId + "/checkItem/" + arr[i].id + "?state=" + state + "&key=" + key + "&token=" + token);
-            xhr.send(data);
-        })
-
-    }
-}
+        event.target.checked ? (state = "complete") : (state = "incomplete");
+        var data = null;
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                console.log(this.responseText);
+            }
+        });
+        xhr.open(
+            "PUT",
+            "https://api.trello.com/1/cards/" +
+            cardId +
+            "/checkItem/" +
+            arr[index].id +
+            "?state=" +
+            state +
+            "&key=" +
+            key +
+            "&token=" +
+            token
+        );
+        xhr.send(data);
+        resolve(fetchArr());
+    });
+};
 
 let addElement = () => {
     let cb = document.createElement("input");
@@ -104,10 +121,11 @@ let addElement = () => {
     newdiv.className = "newdiv";
     span.className = "close";
     var inputValue = document.getElementById("myInput").value;
+    document.getElementById("myInput").value="";
     cb.setAttribute("type", "checkbox");
     cb1.textContent = inputValue;
     span.textContent = "\u00D7";
-    if (inputValue === '') {
+    if (inputValue === "") {
         alert("You must write something!");
     } else {
         document.getElementById("list").appendChild(newdiv);
@@ -121,17 +139,36 @@ let addElement = () => {
                 console.log(this.responseText);
             }
         });
-        arr = fetchArr();
-        deleteElement();
-        xhr.open("POST", "https://api.trello.com/1/checklists/" + checklistId + "/checkItems?name=" + inputValue + "&pos=bottom&checked=false&key=" + key + "&token=" + token);
+        xhr.open(
+            "POST",
+            "https://api.trello.com/1/checklists/" +
+            checklistId +
+            "/checkItems?name=" +
+            inputValue +
+            "&pos=bottom&checked=false&key=" +
+            key +
+            "&token=" +
+            token
+        );
         xhr.send(data);
-
-
+        fetchArr();
     }
 
-    console.log(arr);
-}
+    cb.addEventListener("click", async function (event) {
+        arr = await updateElement(event, arr, arr.length - 1);
+    });
+    span.addEventListener("click", async function (event) {
+        arr = await deleteElement(event, arr, arr.length - 1);
+    });
+};
+
 window.addEventListener("load", async function () {
     arr = await fetchArr();
     await operations(arr);
-})
+});
+let btn = document.querySelector(".search-botton");
+btn.addEventListener("click", async function () {
+    await addElement();
+    arr = await fetchArr();
+
+});
